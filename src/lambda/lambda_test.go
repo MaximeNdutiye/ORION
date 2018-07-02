219b16cb440c3b2fdf2ab8513287a3fe90493bbb
Maxime Ndutiye <maximenduti@gmail.com>
2018-07-02 14:49:05 -0400

    consolodate tests and lambda

--- src/lambda/lambda_test.go
+++ src/lambda/lambda_test.go
@@ -1,8 +1,7 @@
-package main_test
+package lambda
 
 import (
 	"fmt"
-	"lambda"
 	"testing"
 
 	"github.com/aws/aws-lambda-go/events"
@@ -25,7 +24,7 @@ func TestHandler(t *testing.T) {
 	}
 
 	for _, test := range tests {
-		response, err := lambda.Handler(test.request)
+		response, err := Handler(test.request)
 		fmt.Println("Response: ", response)
 		assert.IsType(t, test.err, err)
 		assert.Equal(t, test.expect, response.Body)
