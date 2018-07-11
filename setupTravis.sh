3afb4a1fb05d80a3948cbc478fa3980b4dacb87d
Maxime Ndutiye <maximenduti@gmail.com>
2018-07-11 16:59:15 -0400

    echo path after terraform install

--- setupTravis.sh
+++ setupTravis.sh
@@ -1,2 +1,3 @@
 curl https://releases.hashicorp.com/terraform/0.11.7/terraform_0.11.7_linux_amd64.zip > terraform_0.11.7_linux_amd64.zip && \
-unzip terraform_0.11.7_linux_amd64.zip -d . && export PATH="$PATH:$(pwd)/terraform" && rm -f terraform_0.11.7_linux_amd64.zip
+unzip terraform_0.11.7_linux_amd64.zip -d . && export PATH="$PATH:$HOME/gopath/src/github.com/MaximeNdutiye/ORION/terraform"  \
+rm -f terraform_0.11.7_linux_amd64.zip && echo $PATH
