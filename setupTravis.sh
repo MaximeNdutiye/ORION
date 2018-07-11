767fa4aba3978a27fb77dd10de1a489a1ba0cb6d
Maxime Ndutiye <maximenduti@gmail.com>
2018-07-11 16:51:41 -0400

    safer, build with travis

--- setupTravis.sh
+++ setupTravis.sh
@@ -1,3 +1,2 @@
 curl https://releases.hashicorp.com/terraform/0.11.7/terraform_0.11.7_linux_amd64.zip > terraform_0.11.7_linux_amd64.zip && \
-unzip terraform_0.11.7_linux_amd64.zip -d /bin && \
-rm -f terraform_0.11.7_linux_amd64.zip
+unzip terraform_0.11.7_linux_amd64.zip -d . && export PATH="$PATH:$(pwd)/terraform" && rm -f terraform_0.11.7_linux_amd64.zip
