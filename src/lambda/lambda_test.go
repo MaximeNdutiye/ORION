4b7fc2fa9960347805a27c77f8d0045220444f55
Maxime Ndutiye <maximenduti@gmail.com>
2018-08-27 16:38:49 -0400

    add imports for test

--- src/lambda/lambda_test.go
+++ src/lambda/lambda_test.go
@@ -11,6 +11,8 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/apigateway"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"os"
 	"github.com/stretchr/testify/assert"
 )
 
